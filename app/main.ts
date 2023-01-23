61f7cc99945ec5e8a8013afe09e8997c74065d96
Emre <aka548548@gmail.com>
2023-01-23 17:29:39 +0300

    init

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import {app, BrowserWindow, screen} from 'electron';
+import {app, BrowserWindow, Menu, screen} from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 
@@ -58,7 +58,7 @@ try {
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+  app.on('ready', () => {setTimeout(createWindow, 400); Menu.setApplicationMenu(null)});
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
