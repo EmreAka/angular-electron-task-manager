3ed9f02ed6f8cca97dad8c17f040512bac0115ce
Emre <aka548548@gmail.com>
2023-01-24 23:51:06 +0300

    cpu perf

--- src/app/views/performance-view/components/cpu-performance/cpu-performance.component.ts
+++ src/app/views/performance-view/components/cpu-performance/cpu-performance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PerformanceService } from '../../services/performance.service';
 
 @Component({
   selector: 'app-cpu-performance',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cpu-performance.component.scss']
 })
 export class CpuPerformanceComponent implements OnInit {
+  cpuUsage: number = 0
 
-  constructor() { }
+  cpuResources: any = [{
+    name: "CPU",
+    series: [
+
+    ]
+  }];
+
+
+  view: [number, number] = [700, 300];
+  // options
+  legend: boolean = false;
+  showLabels: boolean = false;
+  animations: boolean = false;
+  xAxis: boolean = false;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = false;
+  showXAxisLabel: boolean = false;
+  xAxisLabel: string = 'Time';
+  yAxisLabel: string = 'Usage';
+  timeline: boolean = false;
+  colorScheme: any = {
+    domain: ['#0081B4', '#FFFBF5', '#FFFBF5', '#FFFBF5', '#FFFBF5', '#FFFBF5'],
+    label: ['#0081B4']
+  };
+
+
+
+  constructor(private performanceService: PerformanceService) { }
 
   ngOnInit(): void {
+    this.performanceService.getCpuUsage().subscribe({
+      next: (value) => {
+        this.cpuUsage = value
+        if (this.cpuResources[0].series.length > 20) {
+          this.cpuResources[0].series.shift()
+        }
+        this.cpuResources[0].series.push({ name: Date().toString(), value: value })
+        this.cpuResources = [...this.cpuResources]
+      }
+    })
   }
 
 }
