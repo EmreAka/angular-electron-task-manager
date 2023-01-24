e535c6e7e3ab9f463558ac872e24a95af4086594
Emre <aka548548@gmail.com>
2023-01-24 14:58:32 +0300

    cleaned up

--- src/app/views/performance-view/performance-view.component.ts
+++ src/app/views/performance-view/performance-view.component.ts
@@ -12,44 +12,6 @@ export class PerformanceViewComponent implements OnInit, OnDestroy {
   cpuUsage: string = ""
   ramUsage: string = ""
 
-  multi: any = [{
-    name: "patates",
-    series: [
-      {
-        name: "sanane",
-        value: 31
-      },
-      {
-        name: "mal",
-        value: 60
-      },
-      {
-        name: "öküz",
-        value: 20
-      },
-      {
-        name: "salak",
-        value: 100
-      },
-    ]
-  }];
-  view: [number, number] = [700, 300];
-  // options
-  legend: boolean = true;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Year';
-  yAxisLabel: string = 'Population';
-  timeline: boolean = true;
-  colorScheme: any = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-  };
-
-
   constructor(private electronService: ElectronService) { }
 
   ngOnDestroy(): void {
@@ -58,14 +20,12 @@ export class PerformanceViewComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.electronService.ipcRenderer.on('cpu-response', (event, data) => {
-      console.log(data.currentLoad);
       const usagePercentage = (data.currentLoad).toFixed(2) + '%';
       this.cpuUsage = usagePercentage
     });
 
     this.electronService.ipcRenderer.on('ram-response', (event, data) => {
       const usagePercentage = (data * 100).toFixed(2) + '%';
-      console.log("RAM usage: " + usagePercentage);
       this.ramUsage = usagePercentage
     });
 
@@ -74,9 +34,4 @@ export class PerformanceViewComponent implements OnInit, OnDestroy {
       this.electronService.ipcRenderer.send('cpu-request');
     }, 500)
   }
-
-  onSelect(event) {
-    console.log(event);
-  }
-
 }
